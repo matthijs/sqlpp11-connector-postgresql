10238434318de616610443342d0d9620f402a31c
Matthijs Möhlmann <matthijs@cacholong.nl>
2014-07-05 14:05:52 +0200

    Add interpreter template to make parameters in prepared statements possible.

---
+++ include/sqlpp11/postgresql/interpreter.h
@@ -0,0 +1,22 @@
+#ifndef SQLPP_POSTGRESQL_INTERPRETER_H
+#define SQLPP_POSTGRESQL_INTERPRETER_H
+
+#include <sqlpp11/interpreter.h>
+#include <sqlpp11/wrap_operand.h>
+#include <sqlpp11/parameter.h>
+
+namespace sqlpp {
+
+	template<typename ValueType, typename NameType>
+		struct interpreter_t<postgresql::context_t, parameter_t<ValueType, NameType>> {
+			using T = parameter_t<ValueType, NameType>;
+
+			static postgresql::context_t &_(const T &t, postgresql::context_t &context) {
+				context << "$" << context.count();
+				context.pop_count();
+				return context;
+			}
+		};
+}
+
+#endif
