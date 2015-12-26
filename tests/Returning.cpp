f2ecff9e0a648e2c6be3041bab279a66103e272a
Matthijs Möhlmann <matthijs@cacholong.nl>
2015-12-26 19:51:48 +0100

    Disable database connection for now.

--- tests/Returning.cpp
+++ tests/Returning.cpp
@@ -9,16 +9,16 @@
 int Returning(int argc, char **argv) {
 
   // Configuration
-  auto config = std::make_shared<sqlpp::postgresql::connection_config>();
+  //auto config = std::make_shared<sqlpp::postgresql::connection_config>();
 
   // DB connection
-  sqlpp::postgresql::connection db(config);
+  //sqlpp::postgresql::connection db(config);
 
   // Model
   test::TabBar t_bar;
-
   auto ins_news = insert_into(t_bar)
 	  .set(t_bar.gamma = true, t_bar.beta = "test")
 	  .returning(t_bar.beta);
+
   return 0;
 }
