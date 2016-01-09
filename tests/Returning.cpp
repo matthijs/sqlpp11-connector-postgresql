309d03760f474e9b88535265bbfc1eaca6cbc396
Matthijs Möhlmann <matthijs@cacholong.nl>
2016-01-09 13:37:38 +0100

    Add more tests, use the MockDb.h from rbock/sqlpp11

--- tests/Returning.cpp
+++ tests/Returning.cpp
@@ -4,6 +4,7 @@
 #include <sqlpp11/postgresql/insert.h>
 //#include <sqlpp11/sqlpp11.h>
 
+#include "MockDb.h"
 #include "Sample.h"
 
 int Returning(int argc, char **argv) {
@@ -14,11 +15,20 @@ int Returning(int argc, char **argv) {
   // DB connection
   //sqlpp::postgresql::connection db(config);
 
+  // Mock DB
+  MockDb db = {};
+  MockDb::_serializer_context_t printer = {};
+
   // Model
   test::TabBar t_bar;
-  auto ins_news = sqlpp::postgresql::insert_into(t_bar)
+  std::cout << serialize(sqlpp::postgresql::insert_into(t_bar)
 	  .set(t_bar.gamma = true, t_bar.beta = "test")
-	  .returning(t_bar.beta);
+	  .returning(t_bar.beta, t_bar.gamma), printer).str() << std::endl;
+  printer.reset();
+
+  std::cout << serialize(sqlpp::postgresql::insert_into(t_bar)
+	  .set(t_bar.gamma = true, t_bar.beta = "test")
+	  .returning(t_bar.beta), printer).str() << std::endl;
 
   return 0;
 }
