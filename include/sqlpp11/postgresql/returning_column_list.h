7a8998e94337f2af2d886596ebfe1e892f5a3960
v.krapivin <v.krapivin@avantize.com>
2018-02-26 11:53:50 +0200

    Fixed compilation problem on gcc

--- include/sqlpp11/postgresql/returning_column_list.h
+++ include/sqlpp11/postgresql/returning_column_list.h
@@ -431,6 +431,7 @@ namespace sqlpp
 
         using _database_t = typename Policies::_database_t;
 
+#ifdef WIN32
         // workaround for msvc bug https://connect.microsoft.com/VisualStudio/Feedback/Details/2173269
         //	  template <typename... T>
         //	  using _check = logic::all_t<(is_selectable_t<T>::value or is_multi_column_t<T>::value)...>;
@@ -438,7 +439,12 @@ namespace sqlpp
         struct _check : logic::all_t<(is_selectable_t<T>::value or is_multi_column_t<T>::value)...>...
         {
         };
-
+#else
+        template <typename... T>
+        struct _check : logic::all_t<(is_selectable_t<T>::value or is_multi_column_t<T>::value)...>
+        {
+        };
+#endif
         template <typename... T>
         static constexpr auto _check_tuple(std::tuple<T...>) -> _check<T...>
         {
