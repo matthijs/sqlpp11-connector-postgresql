52b2e51d0379dba4727e4eb3fe75d46b8f88b8df
Bram Veldhoen <bveldhoen@meyn.com>
2019-01-03 11:48:03 +0100

    Added support for different table schemas.
    - Add option for schemaPattern, which allows to select a (set of) table
    schema(s) for which to generate files.
    - Add option for lower case filenames (and schema folder names).
    - Generate files in a separate folder per table schema_name. This breaks
    existing includes to previously generated files.
    - Namespace includes table schema (namespace::schema_name::table_name).
    This breaks existing code using previously generated types.

--- scripts/ddl2cpp.py
+++ scripts/ddl2cpp.py
@@ -3,6 +3,7 @@
 # Generate C++ structs for the tables.
 
 import argparse
+import errno
 import os
 import sys
 import psycopg2
@@ -16,10 +17,21 @@ parser.add_argument('-p', '--password', dest='password', required=True, help='Po
 parser.add_argument('-d', '--dbname', dest='dbname', required=True, help='PostgreSQL database')
 parser.add_argument('-o', '--output-dir', dest='outputdir', help='Output directory', default='tables/')
 parser.add_argument('-n', '--namespace', dest='namespace', help='C++ namespace', default='model')
+parser.add_argument('-s', '--schemaPattern', dest='schemaPattern', help='LIKE clause pattern for table schema', default='public')
+parser.add_argument('-l', '--lowerCaseFileNames', dest='lowerCaseFileNames', help='true if filenames should be lowercase', default='false')
 args = parser.parse_args()
 
-def _getIncludeGuard(namespace, table):
-    val = re.sub("[^A-Za-z0-9]+", "_", namespace + "_" + table + "_h")
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+def _getIncludeGuard(namespace, tableSchema, tableName):
+    val = re.sub("[^A-Za-z0-9]+", "_", namespace + "_" + tableSchema + "_" + tableName + "_h")
     return val.upper()
 
 def _writeLine(fd, indent, line):
@@ -58,18 +70,36 @@ types = {
 
 nsList = args.namespace.split('::')
 
+# Make output-dir if it doesn't exist
+mkdir_p(args.outputdir)
+
 # Connect to the database and fetch information from the information_schema
 # schema
-conn = psycopg2.connect("host=" + args.host + " user=" + args.user + " password=" + args.password + " dbname=" + args.dbname)
+conn = psycopg2.connect("""host={0} user={1} password={2} dbname={3}""".format(args.host, args.user, args.password, args.dbname))
 curs = conn.cursor()
 
 # First fetch all tables
-curs.execute("""SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'""")
+tableQuery = """SELECT table_schema, table_name FROM information_schema.tables WHERE table_schema LIKE '{0}' ORDER BY table_schema ASC""".format(args.schemaPattern)
+#print tableQuery
+curs.execute(tableQuery)
 tables = curs.fetchall()
 for table in tables:
-    fd = open(os.path.join(args.outputdir, table[0] + '.h'), 'w')
-    _writeLine(fd, 0, "#ifndef " + _getIncludeGuard(args.namespace, table[0]))
-    _writeLine(fd, 0, "#define " + _getIncludeGuard(args.namespace, table[0]))
+    tableSchema = table[0]
+    tableName = table[1]
+
+    schemaDir = tableSchema
+    tableFileName = tableName + ".h"
+    if args.lowerCaseFileNames == "true":
+        schemaDir = tableSchema.lower()
+        tableFileName = tableFileName.lower()
+
+    schemaDir = os.path.join(args.outputdir, schemaDir);
+    mkdir_p(schemaDir)
+
+    tableFileName = os.path.join(schemaDir, tableFileName)
+    fd = open(tableFileName, 'w')
+    _writeLine(fd, 0, "#ifndef " + _getIncludeGuard(args.namespace, tableSchema, tableName))
+    _writeLine(fd, 0, "#define " + _getIncludeGuard(args.namespace, tableSchema, tableName))
     _writeLine(fd, 0, "")
     _writeLine(fd, 0, "")
     _writeLine(fd, 0, "#include <sqlpp11/table.h>")
@@ -79,10 +109,15 @@ for table in tables:
     for ns in nsList:
         _writeLine(fd, 0, "namespace " + ns + " {")
     _writeLine(fd, 0, "")
-    _writeLine(fd, 1, "namespace " + table[0] + "_ {")
+    _writeLine(fd, 0, "namespace " + tableSchema + " {")
+
+    tableColumnsNamespace = tableName + "_"
+    _writeLine(fd, 1, "namespace " + tableColumnsNamespace + " {")
 
     # Fetch all columns for this table
-    curs.execute("""SELECT * FROM information_schema.columns WHERE table_schema = 'public' AND table_name = '%s' ORDER BY table_name ASC, ordinal_position ASC""" % (table[0],))
+    columnQuery = """SELECT * FROM information_schema.columns WHERE table_schema = '{0}' AND table_name = '{1}' ORDER BY table_name ASC, ordinal_position ASC""".format(tableSchema, tableName)
+    #print columnQuery
+    curs.execute(columnQuery)
     columns = curs.fetchall()
     for column in columns:
         _writeLine(fd, 0, "")
@@ -103,7 +138,7 @@ for table in tables:
 
         # Check for an autoincrement value (check if nextval is available and we
         # have a sequence in the form of tablename_columnname_seq)
-        if column[5] and column[5].find("nextval") >= 0 and column[5].find(table[0] + "_" + column[3] + "_seq") >= 0:
+        if column[5] and column[5].find("nextval") >= 0 and column[5].find(tableName + "_" + column[3] + "_seq") >= 0:
             traits+= ", sqlpp::tag::must_not_insert, sqlpp::tag::must_not_update"
 
         # Is column not null and no default value?
@@ -118,34 +153,35 @@ for table in tables:
         _writeLine(fd, 3, traits + ">;")
         _writeLine(fd, 2, "};")
 
-    _writeLine(fd, 1, "}")
+    _writeLine(fd, 1, "} // namespace " + tableColumnsNamespace)
     _writeLine(fd, 0, "")
 
     # Columns are now written as C++ structs, now use them in the definition of
     # the table.
-    _writeLine(fd, 1, "struct " + table[0] + " : sqlpp::table_t<" + table[0] + ",")
+    _writeLine(fd, 1, "struct " + tableName + " : sqlpp::table_t<" + tableName + ",")
     if len(columns) == 1:
-        _writeLine(fd, 4, table[0] + "_::" + columns[0][3].capitalize() + "> {")
+        _writeLine(fd, 4, tableColumnsNamespace + "::" + columns[0][3].capitalize() + "> {")
     else:
         for column in columns[:-1]:
-            _writeLine(fd, 4, table[0] + "_::" + column[3].capitalize() + ",")
-        _writeLine(fd, 4, table[0] + "_::" + columns[-1][3].capitalize() + "> {")
+            _writeLine(fd, 4, tableColumnsNamespace + "::" + column[3].capitalize() + ",")
+        _writeLine(fd, 4, tableColumnsNamespace + "::" + columns[-1][3].capitalize() + "> {")
 
     _writeLine(fd, 2, "using _value_type = sqlpp::no_value_t;")
     _writeLine(fd, 2, "struct _alias_t {")
-    _writeLine(fd, 3, 'static constexpr const char _literal[] = R"("{0}")";'.format(table[0]))
+    _writeLine(fd, 3, 'static constexpr const char _literal[] = R"("{0}"."{1}")";'.format(tableSchema, tableName))
     _writeLine(fd, 3, "using _name_t = sqlpp::make_char_sequence<sizeof(_literal), _literal>;")
     _writeLine(fd, 3, "template<typename T>")
     _writeLine(fd, 4, "struct _member_t {")
-    _writeLine(fd, 5, "T " + table[0] + ";")
-    _writeLine(fd, 5, "T &operator()() { return " + table[0] + "; }");
-    _writeLine(fd, 5, "const T &operator()() const { return " + table[0] + "; }");
+    _writeLine(fd, 5, "T " + tableName + ";")
+    _writeLine(fd, 5, "T &operator()() { return " + tableName + "; }");
+    _writeLine(fd, 5, "const T &operator()() const { return " + tableName + "; }");
     _writeLine(fd, 4, "};")
     _writeLine(fd, 2, "};")
 
     _writeLine(fd, 1, "};")
 
     # end of namespace
+    _writeLine(fd, 0, "} // namespace " + tableSchema)
     for ns in reversed(nsList):
         _writeLine(fd, 0, "} // namespace " + ns)
     _writeLine(fd, 0, "")
