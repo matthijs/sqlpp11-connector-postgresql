47abe3a2146944c004712c3eefdd9e3846dd5b8d
Matthijs Möhlmann <matthijs@cacholong.nl>
2019-07-28 23:40:05 +0200

    add "ON CONFLICT DO UPDATE SET ..."

--- include/sqlpp11/postgresql/on_conflict.h
+++ include/sqlpp11/postgresql/on_conflict.h
@@ -29,6 +29,7 @@
 #define SQLPP_POSTGRESQL_NO_CONFLICT_H
 
 #include <sqlpp11/postgresql/on_conflict_do_nothing.h>
+#include <sqlpp11/postgresql/on_conflict_do_update.h>
 #include <sqlpp11/statement.h>
 
 namespace sqlpp
@@ -37,80 +38,74 @@ namespace sqlpp
 
   namespace postgresql
   {
-    //struct on_conflict_name_t
-    //{
-    //};
-
-    struct on_conflict_t //: public statement_name_t<on_conflict_name_t, tag::is_on_conflict>
+    struct on_conflict_t
     {
-		using _traits = make_traits<no_value_t, tag::is_noop>;
-		using _nodes = sqlpp::detail::type_vector<>;
+      using _traits = make_traits<no_value_t, tag::is_noop>;
+      using _nodes = sqlpp::detail::type_vector<>;
 
-		using _data_t = no_data_t;
+      using _data_t = no_data_t;
 
-		template<typename Policies>
-		struct _impl_t
-		{
-			// workaround for msvc bug https://connect.microsoft.com/VisualStudio/Feedback/Details/2091069
-			_impl_t() = default;
-			_impl_t(const _data_t& data) : _data(data)
-			{
-			}
+      template <typename Policies>
+      struct _impl_t
+      {
+        // workaround for msvc bug https://connect.microsoft.com/VisualStudio/Feedback/Details/2091069
+        _impl_t() = default;
+        _impl_t(const _data_t& data) : _data(data)
+        {
+        }
 
-			_data_t _data;
-		};
+        _data_t _data;
+      };
 
-		template<typename Policies>
-		struct _base_t
-		{
-			using _data_t = no_data_t;
+      template <typename Policies>
+      struct _base_t
+      {
+        using _data_t = no_data_t;
 
-			// workaround for msvc bug https://connect.microsoft.com/VisualStudio/Feedback/Details/2091069
-			template<typename... Args>
-			_base_t(Args&&... args) : on_conflict{std::forward<Args>(args)...}
-			{
-			}
+        // workaround for msvc bug https://connect.microsoft.com/VisualStudio/Feedback/Details/2091069
+        template <typename... Args>
+        _base_t(Args&&... args) : on_conflict{std::forward<Args>(args)...}
+        {
+        }
 
-			_impl_t<Policies> on_conflict;
-			_impl_t<Policies>& operator()()
-			{
-				return on_conflict;
-			}
-			const _impl_t<Policies>& operator()() const
-			{
-				return on_conflict;
-			}
+        _impl_t<Policies> on_conflict;
+        _impl_t<Policies>& operator()()
+        {
+          return on_conflict;
+        }
+        const _impl_t<Policies>& operator()() const
+        {
+          return on_conflict;
+        }
 
-			template<typename T>
-			static auto _get_member(T t) -> decltype(t.on_conflict)
-			{
-				return t.on_conflict;
-			}
+        template <typename T>
+        static auto _get_member(T t) -> decltype(t.on_conflict)
+        {
+          return t.on_conflict;
+        }
 
-			template <typename Check, typename T>
-			using _new_statement_t = new_statement_t<Check, Policies, on_conflict_t, T>;
+        template <typename Check, typename T>
+        using _new_statement_t = new_statement_t<Check, Policies, on_conflict_t, T>;
 
-			using _consistency_check = consistent_t;
+        using _consistency_check = consistent_t;
 
-			// implement do_nothing
-			auto do_nothing() const -> _new_statement_t<consistent_t, on_conflict_do_nothing_t>
-			{
-				return {static_cast<const derived_statement_t<Policies>&>(*this), on_conflict_do_nothing_t::_data_t{}};
-			}
+        // DO NOTHING
+        auto do_nothing() const -> _new_statement_t<consistent_t, on_conflict_do_nothing_t>
+        {
+          return {static_cast<const derived_statement_t<Policies>&>(*this), on_conflict_do_nothing_t::_data_t{}};
+        }
 
-			// TODO: implement do_update
-			//void do_update(...);
-		};
+        // DO UPDATE
+        template <typename... Assignments>
+        auto do_update(Assignments... assignments) const
+            -> _new_statement_t<consistent_t, on_conflict_do_update_t<void, Assignments...>>
+        {
+          // using Check = check_update_static_set_t<Assignments...>;
+          return {static_cast<const derived_statement_t<Policies>&>(*this),
+                  on_conflict_do_update_data_t<void, Assignments...>{std::make_tuple(assignments...)}};
+        }
+      };
     };
-
-    template <typename Database>
-    using blank_on_conflict_t = statement_t<Database, on_conflict_t/*, no_do_nothing_t*/>;
-    // no_do_update>;
-
-    inline blank_on_conflict_t<void> on_conflict()
-    {
-      return {};
-    }
 
     struct no_on_conflict_t
     {
@@ -160,7 +155,7 @@ namespace sqlpp
 
         using _database_t = typename Policies::_database_t;
 
-		// Disable all checks for now
+        // Disable all checks for now
         template <typename... T>
         static constexpr auto _check_tuple(std::tuple<T...>) -> consistent_t /*check_selected_columns_t<T...>*/
         {
@@ -181,7 +176,7 @@ namespace sqlpp
 
         auto on_conflict() const -> _new_statement_t<consistent_t, on_conflict_t>
         {
-			return {static_cast<const derived_statement_t<Policies>&>(*this), on_conflict_t::_data_t{}};
+          return {static_cast<const derived_statement_t<Policies>&>(*this), on_conflict_t::_data_t{}};
         }
       };
     };
---
+++ include/sqlpp11/postgresql/on_conflict_do_update.h
@@ -0,0 +1,138 @@
+/**
+ * Copyright © 2014-2019, Matthijs Möhlmann
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *   Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef SQLPP_POSTGRESQL_ON_CONFLICT_DO_UPDATE_H
+#define SQLPP_POSTGRESQL_ON_CONFLICT_DO_UPDATE_H
+
+#include <sqlpp11/detail/type_set.h>
+#include <sqlpp11/interpret_tuple.h>
+#include <sqlpp11/interpretable_list.h>
+#include <sqlpp11/type_traits.h>
+
+namespace sqlpp
+{
+  SQLPP_VALUE_TRAIT_GENERATOR(is_on_conflict_do_update)
+
+  namespace postgresql
+  {
+    // Assignments data
+    template <typename Database, typename... Assignments>
+    struct on_conflict_do_update_data_t
+    {
+      on_conflict_do_update_data_t(std::tuple<Assignments...> assignments) : _assignments(assignments)
+      {
+      }
+
+      on_conflict_do_update_data_t(const on_conflict_do_update_data_t&) = default;
+      on_conflict_do_update_data_t(on_conflict_do_update_data_t&&) = default;
+      on_conflict_do_update_data_t& operator=(const on_conflict_do_update_data_t&) = default;
+      on_conflict_do_update_data_t& operator=(on_conflict_do_update_data_t&&) = default;
+      ~on_conflict_do_update_data_t() = default;
+
+      std::tuple<Assignments...> _assignments;
+      // interpretable_list_t<Database> _dynamic_assignments;
+    };
+
+    // Use the update_list
+    template <typename Database, typename... Assignments>
+    struct on_conflict_do_update_t
+    {
+      using _traits = make_traits<no_value_t, tag::is_on_conflict_do_update>;
+      using _nodes = sqlpp::detail::type_vector<Assignments...>;
+      using _is_dynamic = is_database<Database>;
+
+      // Data
+      using _data_t = on_conflict_do_update_data_t<Database, Assignments...>;
+
+      // Member implementation with data and methods
+      template <typename Policies>
+      struct _impl_t
+      {
+        // workaround for msvc bug https://connect.microsoft.com/VisualStudio/Feedback/Details/2173269
+        _impl_t() = default;
+        _impl_t(const _data_t& data) : _data(data)
+        {
+        }
+
+        _data_t _data;
+      };
+
+      // Base template to be inherited by the statement
+      template <typename Policies>
+      struct _base_t
+      {
+        using _data_t = on_conflict_do_update_data_t<Database, Assignments...>;
+
+        // workaround for msvc bug https://connect.microsoft.com/VisualStudio/Feedback/Details/2173269
+        template <typename... Args>
+        _base_t(Args&&... args) : assignments{std::forward<Args>(args)...}
+        {
+        }
+
+        _impl_t<Policies> assignments;
+        _impl_t<Policies>& operator()()
+        {
+          return assignments;
+        }
+        const _impl_t<Policies>& operator()() const
+        {
+          return assignments;
+        }
+
+        template <typename T>
+        static auto _get_member(T t) -> decltype(t.assignments)
+        {
+          return t.assignments;
+        }
+
+        // TODO: better checks
+        using _consistency_check = consistent_t;
+      };
+    };
+  }  // namespace postgresql
+
+  template <typename Context, typename Database, typename... Assignments>
+  struct serializer_t<Context, postgresql::on_conflict_do_update_data_t<Database, Assignments...>>
+  {
+    using _serialize_check = consistent_t;
+    using Operand = postgresql::on_conflict_do_update_data_t<Database, Assignments...>;
+
+    static Context& _(const Operand& o, Context& context)
+    {
+      context << " ON CONFLICT DO UPDATE SET ";
+      interpret_tuple(o._assignments, ",", context);
+      /*if (sizeof...(Assignments) and not o._dynamic_assignments.empty())
+      {
+        context << ',';
+      }
+      interpret_tuple(o._dynamic_assignments, ',', context);*/
+      return context;
+    }
+  };
+}  // namespace sqlpp
+
+#endif
