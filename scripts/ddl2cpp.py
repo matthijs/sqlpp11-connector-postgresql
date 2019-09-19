1ff3519af240e8f50bf33ac5f57718b3cbb36a7a
Leo Wu <leowu@no-reply.git>
2019-09-19 15:59:02 -0400

    1. replace asterisk with column names 2. add character mapping

--- scripts/ddl2cpp.py
+++ scripts/ddl2cpp.py
@@ -44,6 +44,7 @@ types = {
     'int': 'integer',
     'bigint': 'bigint',
     'char': 'char_',
+    'character': 'char_',    
     'varchar': 'varchar',
     'character varying': 'varchar',
     'text': 'text',
@@ -114,7 +115,14 @@ for table in tables:
     _writeLine(fd, 1, "namespace " + tableColumnsNamespace + " {")
 
     # Fetch all columns for this table
-    columnQuery = """SELECT * FROM information_schema.columns WHERE table_schema = '{0}' AND table_name = '{1}' ORDER BY table_name ASC, ordinal_position ASC""".format(tableSchema, tableName)
+    columnQuery = """SELECT table_catalog,table_schema,table_name,column_name,ordinal_position,
+    column_default,is_nullable,data_type,character_maximum_length,character_octet_length,
+    numeric_precision,numeric_precision_radix,numeric_scale,datetime_precision,interval_type,
+    interval_precision,character_set_catalog,character_set_schema,character_set_name,collation_catalog,
+    collation_schema,collation_name,domain_catalog,domain_schema,domain_name,udt_catalog,udt_schema,udt_name,scope_catalog,scope_schema,
+    scope_name,maximum_cardinality,dtd_identifier,is_self_referencing,is_identity,identity_generation,identity_start,identity_increment,identity_maximum,
+    identity_cycle,is_generated,generation_expression,is_updatable 
+    FROM information_schema.columns WHERE table_schema = '{0}' AND table_name = '{1}' ORDER BY table_name ASC, ordinal_position ASC""".format(tableSchema, tableName)
     #print columnQuery
     curs.execute(columnQuery)
     columns = curs.fetchall()
